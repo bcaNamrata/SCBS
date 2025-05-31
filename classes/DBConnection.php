daa60c804d7cbee05e140f5d8ca3066beb6dd483
namrata bajracharya <namubazz24@gmail.com>
2025-05-31 21:26:13 +0545

    first push

---
+++ classes/DBConnection.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('DB_SERVER')) {
+    require_once("../initialize.php");
+}
+
+class DBConnection {
+    public $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function __destruct() {
+        // Only close if the connection was successful and is still open
+        if ($this->conn && !$this->conn->connect_error) {
+            $this->conn->close();
+        }
+    }
+}
+?>
